9832263ee4adc6873f30eeae9333d03606986c3c
Vitória Cardoso Alves <163563133+vitoriacardoso14@users.noreply.github.com>
2026-04-29 11:32:36 +0000

    Vitória

---
+++ src/main/java/logisticaentregas/projetocep/ProjetocepApplication.java
@@ -0,0 +1,13 @@
+package logisticaentregas.projetocep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/logisticaentregas/projetocep/ProjetocepApplicationTests.java
@@ -0,0 +1,13 @@
+package logisticaentregas.projetocep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
